5a3e98610ece20f558bd537df2939333f69807f4
FSB-PC <homkkarsx@gmail.com>
2024-03-01 13:38:21 +0300

    Edite users.controller.ts add updated docs

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,7 +7,7 @@ import {
   Patch,
   UseGuards,
 } from '@nestjs/common';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { Role } from '../auth/enums/role.enum';
 import { UsersService } from './users.service';
@@ -19,64 +19,71 @@ import { RoleGuard } from '../auth/guards/role.guard';
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
-  @Get('getAllUsers')
+  @Get('')
   @UseGuards(AuthGuard, RoleGuard)
   @Roles(Role.Admin)
-  @ApiOkResponse()
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiOkResponse({ description: 'List of users fetched successfully' })
   @HttpCode(HttpStatus.OK)
   async getAllUsers() {
     return this.userService.getAllUsers();
   }
 
-  @Get('getUserById/:id')
+  @Get('/:id')
   @UseGuards(AuthGuard)
   @Roles(Role.Admin)
-  @ApiOkResponse()
+  @ApiOperation({ summary: 'Get user by ID' })
+  @ApiOkResponse({ description: 'User fetched successfully' })
   @HttpCode(HttpStatus.OK)
   async getUserById(@Param('id') id: string) {
     return this.userService.getUserById(Number(id));
   }
 
-  @Get('getUserByEmail/:email')
+  @Get('/:email')
   @UseGuards(AuthGuard)
   @Roles(Role.Admin)
-  @ApiOkResponse()
+  @ApiOperation({ summary: 'Get user by email' })
+  @ApiOkResponse({ description: 'User fetched successfully' })
   @HttpCode(HttpStatus.OK)
   async getUserByEmail(@Param('email') email: string) {
     return this.userService.findByEmail(email);
   }
 
-  @Get('getAllUserChild')
+  @Get('child')
   @UseGuards(AuthGuard)
   @Roles(Role.User)
-  @ApiOkResponse()
+  @ApiOperation({ summary: 'Get all user children' })
+  @ApiOkResponse({ description: 'Children fetched successfully' })
   @HttpCode(HttpStatus.OK)
   async getAllUserChild() {
     return true;
   }
 
-  @Get('getUserChildById/:id')
+  @Get('child/:id')
   @UseGuards(AuthGuard)
   @Roles(Role.User)
-  @ApiOkResponse()
+  @ApiOperation({ summary: 'Get user child by ID' })
+  @ApiOkResponse({ description: 'Child fetched successfully' })
   @HttpCode(HttpStatus.OK)
   async getUserChildById(@Param('id') id: string) {
     return id;
   }
 
-  @Patch('activateUser/:id')
+  @Patch(':id/activate')
   @UseGuards(AuthGuard)
   @Roles(Role.Admin)
-  @ApiOkResponse()
+  @ApiOperation({ summary: 'Activate user' })
+  @ApiOkResponse({ description: 'User activated successfully' })
   @HttpCode(HttpStatus.OK)
   async activate(@Param('id') id: string) {
     await this.userService.setUserStatus(Number(id), true);
   }
 
-  @Patch('deactivateUser/:id')
+  @Patch(':id/deactivate')
   @UseGuards(AuthGuard)
   @Roles(Role.Admin)
-  @ApiOkResponse()
+  @ApiOperation({ summary: 'Deactivate user' })
+  @ApiOkResponse({ description: 'User deactivated successfully' })
   @HttpCode(HttpStatus.OK)
   async deactivateUser(@Param('id') id: string) {
     await this.userService.setUserStatus(Number(id), false);
