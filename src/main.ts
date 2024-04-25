adb368bfbbc229d7a63a391b25ba587ce8caa289
zloihach <72695485+zloihach@users.noreply.github.com>
2024-04-25 10:39:10 +0300

    remove splunker

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import * as cors from 'cors';
-import { SpelunkerModule } from 'nestjs-spelunker';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,26 +19,10 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.use(
     cors({
-      origin: 'http://localhost:4200', // Allow requests from this origin
-      credentials: true, // Allow cookies to be sent along with the request
+      origin: 'http://localhost:4200',
+      credentials: true,
     }),
   );
-
-  const tree = SpelunkerModule.explore(app);
-  const root = SpelunkerModule.graph(tree);
-  const edges = SpelunkerModule.findGraphEdges(root);
-  const mermaidEdges = edges
-    .filter(
-      ({ from, to }) =>
-        !(
-          from.module.name === 'ConfigHostModule' ||
-          from.module.name === 'LoggerModule' ||
-          to.module.name === 'ConfigHostModule' ||
-          to.module.name === 'LoggerModule'
-        ),
-    )
-    .map(({ from, to }) => `${from.module.name}-->${to.module.name}`);
-  console.log(`graph TD\n\t${mermaidEdges.join('\n\t')}`);
 
   await app.listen(3010);
 }
