26ca257c37ff1d617a3f801199dbdce4ce25e82a
zloihach <72695485+zloihach@users.noreply.github.com>
2024-05-17 19:41:04 +0300

    add logger

--- src/publication/publication.service.ts
+++ src/publication/publication.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { Publication } from '@prisma/client';
 import { CreatePublicationBodyDto } from './dto/createPublicationDto';
 import { EditPublicationBodyDto } from './dto/editPublicationDto';
@@ -12,19 +12,22 @@ import { RedisService } from '../../redis/redis.service';
 
 @Injectable()
 export class PublicationService implements IPublicationService {
+  private readonly logger = new Logger(PublicationService.name);
+  private readonly cacheKeyPrefix = 'publication:';
+
   constructor(
     private readonly db: DbService,
     private readonly fileService: FileService,
     private readonly redisService: RedisService,
   ) {}
 
-  private readonly cacheKeyPrefix = 'publication:';
-
   async createPublication(
     createPublicationBodyDto: CreatePublicationBodyDto,
   ): Promise<Publication> {
     const { image, ...publicationData } = createPublicationBodyDto;
-    const imageUrl: string | null = image ? await this.uploadImage(image) : null;
+    const imageUrl: string | null = image
+      ? await this.uploadImage(image)
+      : null;
 
     const publication = await this.db.publication.create({
       data: {
@@ -35,7 +38,7 @@ export class PublicationService implements IPublicationService {
       },
     });
 
-    await this.redisService.delete(this.cacheKeyPrefix + 'all:*');
+    await this.clearCacheForPublications();
     return publication;
   }
 
@@ -45,7 +48,9 @@ export class PublicationService implements IPublicationService {
     const { id, image, ...publicationData } = editPublicationDto;
     await this.findPublicationById(id);
 
-    const imageUrl: string | null = image ? await this.uploadImage(image) : null;
+    const imageUrl: string | null = image
+      ? await this.uploadImage(image)
+      : null;
 
     const updatedPublication = await this.db.publication.update({
       where: { id },
@@ -60,7 +65,12 @@ export class PublicationService implements IPublicationService {
       this.cacheKeyPrefix + updatedPublication.id,
       JSON.stringify(updatedPublication),
     );
-    await this.redisService.delete(this.cacheKeyPrefix + 'all:*');
+    this.logger.log(
+      `Redis: Updated cache for key ${
+        this.cacheKeyPrefix + updatedPublication.id
+      }`,
+    );
+    await this.clearCacheForPublications();
 
     return updatedPublication;
   }
@@ -83,19 +93,27 @@ export class PublicationService implements IPublicationService {
       this.cacheKeyPrefix + updatedPublication.id,
       JSON.stringify(updatedPublication),
     );
-    await this.redisService.delete(this.cacheKeyPrefix + 'all:*');
+    this.logger.log(
+      `Redis: Updated cache for key ${
+        this.cacheKeyPrefix + updatedPublication.id
+      }`,
+    );
+    await this.clearCacheForPublications();
 
     return updatedPublication;
   }
 
   async getAllPublications({
-                             page,
-                             limit,
-                           }: PaginationQueryDto): Promise<Publication[]> {
+    page,
+    limit,
+  }: PaginationQueryDto): Promise<Publication[]> {
     const cacheKey = `${this.cacheKeyPrefix}all:${page}:${limit}`;
     const cachedPublications = await this.redisService.get(cacheKey);
 
     if (cachedPublications) {
+      this.logger.log(
+        `Redis: Fetched publications from cache with key ${cacheKey}`,
+      );
       return JSON.parse(cachedPublications);
     }
 
@@ -105,6 +123,9 @@ export class PublicationService implements IPublicationService {
     });
 
     await this.redisService.insert(cacheKey, JSON.stringify(publications));
+    this.logger.log(
+      `Redis: Inserted publications into cache with key ${cacheKey}`,
+    );
     return publications;
   }
 
@@ -113,12 +134,18 @@ export class PublicationService implements IPublicationService {
     const cachedPublication = await this.redisService.get(cacheKey);
 
     if (cachedPublication) {
+      this.logger.log(
+        `Redis: Fetched publication from cache with key ${cacheKey}`,
+      );
       return JSON.parse(cachedPublication);
     }
 
     const publication = await this.findPublicationById(id);
 
     await this.redisService.insert(cacheKey, JSON.stringify(publication));
+    this.logger.log(
+      `Redis: Inserted publication into cache with key ${cacheKey}`,
+    );
     return publication;
   }
 
@@ -128,21 +155,22 @@ export class PublicationService implements IPublicationService {
     const { keyword, isActive } = searchPublicationDto;
 
     const whereClause: any = {};
-
     if (keyword !== undefined) {
-      whereClause.full_title = {
-        contains: keyword,
-      };
+      whereClause.full_title = { contains: keyword };
     }
-
     if (isActive !== undefined) {
       whereClause.is_active = isActive;
     }
 
-    const cacheKey = `${this.cacheKeyPrefix}search:${JSON.stringify(whereClause)}`;
+    const cacheKey = `${this.cacheKeyPrefix}search:${JSON.stringify(
+      whereClause,
+    )}`;
     const cachedPublications = await this.redisService.get(cacheKey);
 
     if (cachedPublications) {
+      this.logger.log(
+        `Redis: Fetched search results from cache with key ${cacheKey}`,
+      );
       return JSON.parse(cachedPublications);
     }
 
@@ -151,6 +179,9 @@ export class PublicationService implements IPublicationService {
     });
 
     await this.redisService.insert(cacheKey, JSON.stringify(publications));
+    this.logger.log(
+      `Redis: Inserted search results into cache with key ${cacheKey}`,
+    );
     return publications;
   }
 
@@ -166,9 +197,7 @@ export class PublicationService implements IPublicationService {
   }
 
   private async findPublicationById(id: number): Promise<Publication> {
-    const publication = await this.db.publication.findUnique({
-      where: { id },
-    });
+    const publication = await this.db.publication.findUnique({ where: { id } });
     if (!publication) {
       throw new NotFoundException(`Publication with ID ${id} not found`);
     }
@@ -180,11 +209,24 @@ export class PublicationService implements IPublicationService {
     const cachedCount = await this.redisService.get(cacheKey);
 
     if (cachedCount) {
+      this.logger.log(
+        `Redis: Fetched publication count from cache with key ${cacheKey}`,
+      );
       return parseInt(cachedCount, 10);
     }
 
     const count = await this.db.publication.count();
     await this.redisService.insert(cacheKey, count.toString());
+    this.logger.log(
+      `Redis: Inserted publication count into cache with key ${cacheKey}`,
+    );
     return count;
+  }
+
+  private async clearCacheForPublications() {
+    await this.redisService.delete(this.cacheKeyPrefix + 'all:*');
+    this.logger.log(
+      `Redis: Cleared cache for key ${this.cacheKeyPrefix + 'all:*'}`,
+    );
   }
 }
