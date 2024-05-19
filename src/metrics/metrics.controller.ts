4d4a51b5598a6c319dff9b1d2395ece3da4adbbd
zloihach <72695485+zloihach@users.noreply.github.com>
2024-05-19 13:42:35 +0300

    add alertmanager && cadvisor

--- src/metrics/metrics.controller.ts
+++ src/metrics/metrics.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { collectDefaultMetrics, Registry } from 'prom-client';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('metrics')
+@ApiTags('Metrics')
 export class MetricsController {
   private readonly register: Registry;
 
@@ -12,8 +14,15 @@ export class MetricsController {
   }
 
   @Get()
-  async getMetrics(@Res() res: Response) {
-    res.set('Content-Type', this.register.contentType);
-    res.end(await this.register.metrics());
+  @ApiOperation({ summary: 'Get Prometheus metrics' })
+  @ApiResponse({ status: 200, description: 'Metrics fetched successfully' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
+  async getMetrics(@Res() res: Response): Promise<void> {
+    try {
+      res.set('Content-Type', this.register.contentType);
+      res.end(await this.register.metrics());
+    } catch (error) {
+      res.status(500).send('Internal server error');
+    }
   }
 }
